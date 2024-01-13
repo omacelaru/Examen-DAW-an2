8c203bee19515475a343632bc333cd93d6ce3009
Octavian Andrei Măcelaru <octavian-andrei.macelaru@s.unibuc.ro>
2024-01-13 21:27:39 +0200

    add BaseEntity

---
+++ Examen/Models/Base/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Examen.Models.Base
+{
+    public class BaseEntity : IBaseEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+    }
+}
---
+++ Examen/Models/Base/IBaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Examen.Models.Base
+{
+    public interface IBaseEntity
+    {
+        Guid Id { get; set; }
+        DateTime CreatedAt { get; set; }
+        DateTime UpdatedAt { get; set; }
+    }
+}
