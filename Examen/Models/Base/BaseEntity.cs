e24847d0724dc84583494f77a57ee1a850c0b381
Octavian Andrei Măcelaru <octavian-andrei.macelaru@s.unibuc.ro>
2024-01-13 21:47:57 +0200

    fix constraint

--- Examen/Models/Base/BaseEntity.cs
+++ Examen/Models/Base/BaseEntity.cs
@@ -1,8 +1,15 @@
-﻿namespace Examen.Models.Base
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Examen.Models.Base
 {
     public class BaseEntity : IBaseEntity
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid Id { get; set; }
+
+        [Required]
         public DateTime CreatedAt { get; set; } = DateTime.Now;
         public DateTime UpdatedAt { get; set; } = DateTime.Now;
     }
