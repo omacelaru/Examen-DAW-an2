d3e222a29bedd41db5153b8e3700affaa68d93c7
Octavian Andrei Măcelaru <octavian-andrei.macelaru@s.unibuc.ro>
2024-01-13 20:57:43 +0200

    add ServiceExtensions

---
+++ Examen/Helpers/Extensions/ServiceExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Examen.Helpers.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            //services.AddTransient<IRepository, Repository>();
+            return services;
+        }
+
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            //services.AddTransient<IService,Service>();
+            return services;
+        }
+    }
+}
